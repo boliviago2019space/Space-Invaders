1e01cb6213132e4b4f1505cd751fe9093aee0aa5
boliviago2019space <boliviago2019@gmail.com>
2026-08-10 16:30:44 -0400

    Update spaceinvaders.config.js

--- spaceinvaders.config.js
+++ spaceinvaders.config.js
@@ -1,20 +1,20 @@
 export default {
   models: {
-    invader1: './assets/models/invader1.babylon',
-    invader2: './assets/models/invader2.babylon',
-    invader3: './assets/models/invader3.babylon',
-    motherShip: './assets/models/motherShip.babylon',
-    defender: './assets/models/defender.babylon',
-    bunker: './assets/models/bunker.babylon',
+    invader1: 'assets/models/invader1.babylon',
+    invader2: 'assets/models/invader2.babylon',
+    invader3: 'assets/models/invader3.babylon',
+    motherShip: 'assets/models/motherShip.babylon',
+    defender: 'assets/models/defender.babylon',
+    bunker: 'assets/models/bunker.babylon',
   },
   sounds: {
-    shoot: './assets/sounds/shoot.wav',
-    invaderKilled: './assets/sounds/invaderKilled.wav',
-    explosion: './assets/sounds/explosion.wav',
-    fastInvader1: './assets/sounds/fastInvader1.wav',
-    fastInvader2: './assets/sounds/fastInvader2.wav',
-    fastInvader3: './assets/sounds/fastInvader3.wav',
-    fastInvader4: './assets/sounds/fastInvader4.wav',
-    ufoHighpitch: './assets/sounds/ufoHighpitch.wav'
+    shoot: 'assets/sounds/shoot.wav',
+    invaderKilled: 'assets/sounds/invaderKilled.wav',
+    explosion: 'assets/sounds/explosion.wav',
+    fastInvader1: 'assets/sounds/fastInvader1.wav',
+    fastInvader2: 'assets/sounds/fastInvader2.wav',
+    fastInvader3: 'assets/sounds/fastInvader3.wav',
+    fastInvader4: 'assets/sounds/fastInvader4.wav',
+    ufoHighpitch: 'assets/sounds/ufoHighpitch.wav'
   }
 }
