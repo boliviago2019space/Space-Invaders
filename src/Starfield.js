b88b4bce4c072b700dc766539e7f27635a8e4f00
boliviago2019space <boliviago2019@gmail.com>
2026-08-10 17:27:07 -0400

    Update Starfield.js

--- src/Starfield.js
+++ src/Starfield.js
@@ -4,17 +4,20 @@ import { Color4, ParticleSystem, Texture, Vector3 } from "@babylonjs/core";
 export class Starfield {
   constructor(scene) {
     this.scene = scene;
-    
-    // Obtener la configuración de estrellas con valores por defecto si no existen
-    const starConfig = spaceinvadersConfig.starfield || {};
-    const starCount = starConfig.numberOfStars || starConfig.numberofStars || 500;
 
-    const starfield = new ParticleSystem("particles", starCount, this.scene);
+    // Proteccion ante objetos o propiedades indefinidas
+    const starfieldConfig = spaceinvadersConfig && spaceinvadersConfig.starfield 
+      ? spaceinvadersConfig.starfield 
+      : { numberOfStars: 500, numberofStars: 500 };
+
+    const totalStars = starfieldConfig.numberOfStars || starfieldConfig.numberofStars || 500;
+
+    const starfield = new ParticleSystem("particles", totalStars, this.scene);
     starfield.particleTexture = new Texture("./assets/images/star.png", this.scene);
     starfield.emitter = new Vector3(0, 0, 0);
     starfield.minEmitBox = new Vector3(-1000, -1000, 1000);
     starfield.maxEmitBox = new Vector3(1000, 1000, 1000);
-    
+
     starfield.color1 = new Color4(1, 1, 1, 1);
     starfield.color2 = new Color4(1, 1, 1, 1);
     starfield.colorDead = new Color4(0, 0, 0, 0.2);
@@ -25,7 +28,7 @@ export class Starfield {
     starfield.minLifeTime = 999999;
     starfield.maxLifeTime = 999999;
 
-    starfield.emitRate = starCount;
+    starfield.emitRate = totalStars;
 
     starfield.gravity = new Vector3(0, 0, 0);
     starfield.direction1 = new Vector3(0, 0, 0);
